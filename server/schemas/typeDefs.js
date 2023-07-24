90122b0de01699b55b1514e6496b63ff7d68fe29
dionkb <dionkbaskara@gmail.com>
2023-07-23 22:19:31 -0400

    added basic typeDefs and resolver code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,40 @@
 // const { gql } = require('apollo-server-express');
 
 // const typeDefs = gql`
-//     type TODO: {
-
+//     type User {
+//         _id: ID!
+//         username: String!
+//         email: String!
+//         password: String!
+//         savedBooks: [Book]
 //     }
 
-//     type TODO: {
+//     type Book {
+//         _id: ID!
+//         authors: [String]
+//         description: String!
+//         bookId: String!
+//         image: String
+//         link: String
+//         title: String!
+//     }
+
+//     type Auth {
 
 //     }
 
 //     type Query {
-//         TODO:
+//         user(username: String!): User
+//     }
+
+//     type Mutation {
+//         createUser(username: String!, email: String!, password: String!): Auth
+//         login(email: String!, password: String!): Auth
+//         saveBook(FIXME:): ???
+//         deleteBook(bookId: ID!): Book
 //     }
 // `;
 
 // module.exports = typeDefs;
+
+// ADD ALL OF THIS BACK IN ONCE READY TO TRANSITION OVER
