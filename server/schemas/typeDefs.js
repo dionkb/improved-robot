b85d7ea0cc56254f347beccb745e3db05f17a9c0
dionkb <dionkbaskara@gmail.com>
2023-07-23 21:55:55 -0400

    see comment

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+// const { gql } = require('apollo-server-express');
+
+// const typeDefs = gql`
+//     type TODO: {
+
+//     }
+
+//     type TODO: {
+
+//     }
+
+//     type Query {
+//         TODO:
+//     }
+// `;
+
+// module.exports = typeDefs;
