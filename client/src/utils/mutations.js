0c4bafac4bf5f5b189074dfc91d6aaf605480481
dionkb <dionkbaskara@gmail.com>
2023-07-23 22:58:41 -0400

    added basic code and file structure to client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+// import { gql } from '@apollo/client';
+
+// export const LOGIN_USER = gql`
+//     mutation loginUser(...) {
+        
+//     }
+// `;
+
+// export const ADD_USER = gql`
+//     mutation addUser(...) {
+        
+//     }
+// `;
+
+// export const SAVE_BOOK = gql`
+//     mutation saveBook(...) {
+        
+//     }
+// `;
+
+// export const REMOVE_BOOK = gql`
+//     mutation removeBook(...) {
+        
+//     }
+// `;
+
+// TODO: Add all the code in, then uncomment it all out once ready to transition to apollo
